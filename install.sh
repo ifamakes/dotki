feaa98ed2348e6638f425141be8ed89bc6a934a1
Alicja <k2l8m11n2@gmail.com>
2020-05-20 17:17:10 +0200

    Add youtube-dl

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ ln -s $DOTKI/init.vim .config/nvim/init.vim
 mkdir -p .config/nvim/autoload
 ln -s $DOTKI/.plug.vim .config/nvim/autoload/plug.vim
 ln -s $DOTKI/gitconfig .gitconfig
+mkdir -p .config/youtube-dl
+ln -s $DOTKI/youtube-dl .config/youtube-dl/config
