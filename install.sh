e0b68167799893c9b53b5b9ff552fdf2dd0a07b1
Alicja <k2l8m11n2@gmail.com>
2020-06-04 13:16:57 +0200

    Progressify the curls

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DOTKI="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if ! [[ -x "$(command -v exa)" ]]; then
 	cd $(mktemp -d)
-	curl https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip -OJL
+	curl https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip -#OJL
 	unzip -q exa-linux-x86_64-0.9.0.zip
 	sudo mv exa-linux-x86_64 /usr/local/bin/exa
 	rm -r $(pwd)
@@ -18,7 +18,7 @@ mkdir -p .config/nvim
 ln -s $DOTKI/init.vim .config/nvim/init.vim 2>/dev/null
 mkdir -p .config/nvim/autoload
 if ! [[ -f .config/nvim/autoload/plug.vim ]]; then
-	curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -Lo .config/nvim/autoload/plug.vim
+	curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -#Lo .config/nvim/autoload/plug.vim
 	nvim +PlugInstall +qa
 fi
 
