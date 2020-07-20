932ddb0bdbe5f5e5e7466b6e07f87c7a493b371a
Alicja <k2l8m11n2@gmail.com>
2020-07-20 23:38:27 +0200

    Don't install exa and run nvim by default

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dotki
 ## how do i
 0. clone somewhere
-1. install `zsh`, `curl`, `sudo`, `neovim` and `unzip`
+1. install `zsh`, `curl` and `sudo`
 2. `exec ./install.sh`
 3. profit
--- install.sh
+++ install.sh
@@ -2,14 +2,6 @@
 
 DOTKI="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if ! [[ -x "$(command -v exa)" ]]; then
-	cd $(mktemp -d)
-	curl https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip -#OJL
-	unzip -q exa-linux-x86_64-0.9.0.zip
-	sudo mv exa-linux-x86_64 /usr/local/bin/exa
-	rm -r $(pwd)
-fi
-
 cd ~
 ln -s $DOTKI/zshrc .zshrc 2>/dev/null
 sudo chsh $(whoami) -s /bin/zsh
@@ -19,7 +11,7 @@ ln -s $DOTKI/init.vim .config/nvim/init.vim 2>/dev/null
 mkdir -p .config/nvim/autoload
 if ! [[ -f .config/nvim/autoload/plug.vim ]]; then
 	curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -#Lo .config/nvim/autoload/plug.vim
-	nvim +PlugInstall +qa
+	#nvim +PlugInstall +qa
 fi
 
 ln -s $DOTKI/gitconfig .gitconfig 2>/dev/null
